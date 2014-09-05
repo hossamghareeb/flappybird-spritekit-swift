666cad4970c95405f519c5d5c7a5f7ced3fb14a4
Hossam Ghareeb <hossam.ghareb@gmail.com>
2014-09-06 02:11:02 +0300

    Add resources files Make a reference to the bird that created in scene design

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -9,8 +9,30 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    //This variable is used to know if the game started or not
+    var gameStarted = false
+
+    var bird : SKSpriteNode
+    
+    init(coder aDecoder: NSCoder!)
+    {
+        bird = SKSpriteNode();
+        super.init(coder: aDecoder);
+    }
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        
+        //Scene physics, create edge loop
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        
+        //our bird
+        bird = self.childNodeWithName("bird") as SKSpriteNode
+        bird.physicsBody.density = 1.5;
+        bird.physicsBody.dynamic = false
+        
+
         
     }
     
